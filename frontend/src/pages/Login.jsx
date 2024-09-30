4d426e594eb7cff1284659b49525ebbff31c338d
Szymon <szymonnie2891@gmail.com>
2024-09-30 09:30:29 +0200

    Rewriting to Angular

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Login.css'
+import Logo_Big_Black from '../assets/Logo_Big_Black.png'
+
+const Login = () => {
+  return (
+    <div className='login-page-container'>
+      <img src="" alt="" className="login-page-header" />
+    </div>
+  )
+}
+
+export default Login
